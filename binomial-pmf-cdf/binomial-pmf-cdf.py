1931de84d9781e0c85e0565c719c0436ac262d6f
Tran Hung Thinh <148307463+TranThinh2003@users.noreply.github.com>
2026-03-18 10:47:32 +0700

    Add solution for binomial-pmf-cdf

---
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -0,0 +1,16 @@
+"""
+ *     author:  Shinomiyaaa
+ *     created: 18.03.2026 10:47:18
+"""
+import numpy as np
+from scipy.special import comb
+
+def binomial_pmf_cdf(n, p, k):
+    """
+    Compute Binomial PMF and CDF.
+    """
+    # Write code here
+    pmf = comb(n, k) * (p ** k) * ((1 - p) ** (n - k))
+    cdf = sum(comb(n, i) * (p ** i) * ((1 - p) ** (n - i)) for i in range(k + 1))
+    return pmf, cdf
+    pass
