09b9760c60238ac065b76c2eda26a301ac0f29b1
Tran Hung Thinh <148307463+TranThinh2003@users.noreply.github.com>
2026-03-27 09:33:56 +0700

    Update solution for batch-normalization

--- batch-normalization/batch-normalization.py
+++ batch-normalization/batch-normalization.py
@@ -1,6 +1,6 @@
 """
  *     author:  Shinomiyaaa
- *     created: 04.03.2026 22:36:45
+ *     created: 27.03.2026 09:33:55
 """
 import numpy as np
 
@@ -12,22 +12,16 @@ def batch_norm_forward(x, gamma, beta, eps=1e-5):
     x = np.array(x)
     gamma = np.array(gamma)
     beta = np.array(beta)
-    
     if x.ndim == 2:
-        # (N, D)
-        mean = np.mean(x, axis=0, keepdims=True)
-        var = np.var(x, axis=0, keepdims=True)
-        
+        mean = np.mean(x,axis=0,keepdims=True)
+        var = np.var(x,axis=0,keepdims=True)
     elif x.ndim == 4:
-        # (N, C, H, W)
-        mean = np.mean(x, axis=(0, 2, 3), keepdims=True)
-        var = np.var(x, axis=(0, 2, 3), keepdims=True)
-        if gamma.ndim == 1:
-            gamma = gamma.reshape(1, -1, 1, 1)
-        if beta.ndim == 1:
-            beta = beta.reshape(1, -1, 1, 1)
-
+        mean = np.mean(x,axis=(0,2,3),keepdims=True)
+        var = np.var(x,axis=(0,2,3),keepdims=True)
+        
+        if gamma.ndim == 1: gamma = gamma.reshape(1,-1,1,1)
+        if beta.ndim == 1: beta = beta.reshape(1,-1,1,1)
+    
     x_hat = (x - mean) / np.sqrt(var + eps)
-    ans = gamma * x_hat + beta
-
-    return ans
+    return gamma * x_hat + beta
+    pass
