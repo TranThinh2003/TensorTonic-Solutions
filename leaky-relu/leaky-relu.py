f1a8214bd8098c621147d65eab6cb8f26e3c9c22
Tran Hung Thinh <148307463+TranThinh2003@users.noreply.github.com>
2026-04-09 08:52:33 +0700

    Update solution for leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -1,6 +1,6 @@
 """
- *     author:  Shinomiyaaa
- *     created: 11.03.2026 09:15:08
+ *     author:  _Shinomiyaa_
+ *     created: 09.04.2026 08:52:24
 """
 import numpy as np
 
@@ -9,7 +9,7 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     # Write code here
-    x = np.asarray(x)
-    x = np.array([i * alpha if i < 0 else i for i in x])
+    x = np.asarray(x,dtype=np.float64)
+    x = np.array([i if i >=0 else alpha * i for i in x])
     return x
     pass
