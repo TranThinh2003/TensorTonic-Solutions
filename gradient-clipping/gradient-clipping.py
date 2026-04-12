e0e0f4231d6621585a68b4b219fb0d5c646e7ce9
Tran Hung Thinh <148307463+TranThinh2003@users.noreply.github.com>
2026-04-12 12:20:38 +0700

    Add solution for gradient-clipping

---
+++ gradient-clipping/gradient-clipping.py
@@ -0,0 +1,16 @@
+"""
+ *     author:  _Shinomiyaa_
+ *     created: 12.04.2026 12:14:32
+"""
+import numpy as np
+
+def clip_gradients(g, max_norm):
+    """
+    Clip gradients using global norm clipping.
+    """
+    # Write code here
+    g = np.asarray(g)
+    if max_norm <= 0: return g
+    l2_norm = np.sqrt(np.sum(g ** 2))
+    return g * (max_norm / l2_norm) if l2_norm > max_norm else g
+    pass
