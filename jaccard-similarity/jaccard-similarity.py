cdccf46da0c75fb7d6084a1b219918c00437b0fa
Tran Hung Thinh <148307463+TranThinh2003@users.noreply.github.com>
2026-04-15 07:27:24 +0700

    Update solution for jaccard-similarity

--- jaccard-similarity/jaccard-similarity.py
+++ jaccard-similarity/jaccard-similarity.py
@@ -1,16 +1,16 @@
 """
- *     author:  Shinomiyaaa
- *     created: 10.03.2026 12:04:42
+ *     author:  _Shinomiyaa_
+ *     created: 15.04.2026 07:26:52
 """
-import numpy as np
 def jaccard_similarity(set_a, set_b):
     """
     Compute the Jaccard similarity between two item sets.
     """
+    # Write code here
     set_a = set(set_a)
     set_b = set(set_b)
-    # Write code here
-    intersection = set_a.intersection(set_b)
     union = set_a.union(set_b)
+    intersection = set_a.intersection(set_b)
     if not union: return 0.0  # Avoid division by zero
     return len(intersection) / len(union)
+    
