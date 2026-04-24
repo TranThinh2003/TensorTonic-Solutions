25e70ae0e590ca7b83522e215e4f5133ea2ffa62
Tran Hung Thinh <148307463+TranThinh2003@users.noreply.github.com>
2026-04-24 11:17:52 +0700

    Update solution for matrix-normalization

--- matrix-normalization/matrix-normalization.py
+++ matrix-normalization/matrix-normalization.py
@@ -1,6 +1,6 @@
 """
- *     author:  Shinomiyaaa
- *     created: 20.03.2026 00:22:51
+ *     author:  _Shinomiyaa_
+ *     created: 24.04.2026 11:14:41
 """
 import numpy as np
 
@@ -10,15 +10,16 @@ def matrix_normalization(matrix, axis=None, norm_type='l2'):
     """
     # Write code here
     try:
-        if norm_type not in ('l1', 'l2', 'max', 'inf'): return None
-
-        mat = np.array(matrix, dtype=float)
-        if mat.size == 0 or mat.ndim == 0 or mat.ndim > 2: return None
-        
-        if norm_type == 'l2': norm = np.sqrt(np.sum(mat**2, axis=axis, keepdims=True))
-        elif norm_type == 'l1': norm = np.sum(np.abs(mat), axis=axis, keepdims=True)
-        elif norm_type in ('max', 'inf'): norm = np.max(np.abs(mat), axis=axis, keepdims=True)
-            
-        norm = np.where(norm == 0, 1.0, norm)
+        if norm_type not in ('l1', 'l2','max','inf'): return None
+        mat = np.array(matrix, dtype=np.float64)
+        if mat.size == 0 or mat.ndim > 2: return None
+        if norm_type == 'l1':
+            norm = np.sum(np.abs(mat), axis=axis, keepdims=True)
+        elif norm_type == 'l2':
+            norm = np.sqrt(np.sum(mat**2, axis=axis, keepdims=True))
+        elif norm_type in ('max', 'inf'):
+            norm = np.max(np.abs(mat), axis=axis, keepdims=True)
+        norm = np.where(norm == 0, 1.0, norm)  # Avoid division by zero
         return mat / norm
     except Exception: return None
+    pass
